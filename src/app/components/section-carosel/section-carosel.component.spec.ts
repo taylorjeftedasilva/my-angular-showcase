f1a640251511fe906cfcb8aa9f234e4d663af59a
Taylor <taylorjeftedasilva@gmail.com>
2024-08-10 09:33:46 -0300

    adesão de linha no final de arquivos html

--- src/app/components/section-carosel/section-carosel.component.spec.ts
+++ src/app/components/section-carosel/section-carosel.component.spec.ts
@@ -1,21 +1,21 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { SectionCaroselComponent } from './section-carosel.component';
+import { SectionCaroselComponent } from './section-carosel.component';
 
-// describe('SectionCaroselComponent', () => {
-//   let component: SectionCaroselComponent;
-//   let fixture: ComponentFixture<SectionCaroselComponent>;
+describe('SectionCaroselComponent', () => {
+  let component: SectionCaroselComponent;
+  let fixture: ComponentFixture<SectionCaroselComponent>;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [SectionCaroselComponent]
-//     });
-//     fixture = TestBed.createComponent(SectionCaroselComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SectionCaroselComponent]
+    });
+    fixture = TestBed.createComponent(SectionCaroselComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
